7bf39f5e649e5bb4082dca946ca1a271382a066a
Samuel K <mrsosaki@posteo.net>
2021-06-20 15:13:05 +0100

    Rewrite this bad boi right here

--- todo.py
+++ todo.py
@@ -2,23 +2,13 @@
 from os import replace
 import time
 
-print("Welcome to your TODO List")
-time.sleep(1)
-print("Commands:")
-time.sleep(0.25)
-print("1: Show Pending")
-time.sleep(0.25)
-print("2: Add to List")
-time.sleep(0.25)
-print("3: Remove from List")
-selection = input(": ")
 
 def todo_show():
 
-    #getting list
+    # getting list
     f = open("./list.txt", 'r')
 
-    #reaing list
+    # reaing list
     todolist = f.read()
     print(todolist)
     f.close()
@@ -26,50 +16,66 @@ def todo_show():
 
 def todo_new():
 
-    #getting list
+    # getting list
     f = open("./list.txt", 'a')
 
     new_todo_task = input("Name of new Task\n : ")
 
-    #writing task to newline in file
+    # writing task to newline in file
     f.write(new_todo_task + "\n")
     f.close()
 
-def todo_remove():
 
-    #getting list
+def todo_remove():
+    # getting list
     f = open("./list.txt", "r+")
 
+    # lines = f.read()
 
-    lines = f.read()
+    # putting contents of file in a list 
+    todo_list = []
+    for line in f:
+        line = line.replace('\n', '')
 
-
-    #putting contents of file in a list variable
-    todo_list = [lines]
+        todo_list.append(line)
     f.close()
-    todo_list = str(todo_list)
-
-    #deleting unused characters
-    todo_list = todo_list.replace("n", "\n")
-    todo_list = todo_list.replace('[', "")
-    todo_list = todo_list.replace(']', "")
-    todo_list = todo_list.replace("'", "")
-
-    todo_remove_input = input("TODO Name\n: ")
     
-    todo_list.replace(todo_remove_input, " ")
-    print(todo_list)
+    id = 0
+    for todo in todo_list:
+        print(f"{id}. {todo}")
+        id += 1
 
+    todo_remove_index = int(input("TODO ID: "))
+    todo_list.pop(todo_remove_index)    
+    
+    todo_list_str = '\n'.join(todo_list)
+    f = open('./list.txt', 'w')
+    f.write(todo_list_str)
+    print("Successfully writed file!")
+    f.close()
 
-#deciding task on selecion
+todo_remove()
+# deciding task on selecion
 
-if selection == "1":
-    todo_show()
-elif selection == "2":
-    todo_new()
-elif selection == "3":
-    # Showing the todo list
-    todo_show()
-    todo_remove()
+def main():
+    print("Welcome to your TODO List")
+    time.sleep(1)
+    print("Commands:")
+    time.sleep(0.25)
+    print("1: Show Pending")
+    time.sleep(0.25)
+    print("2: Add to List")
+    time.sleep(0.25)
+    print("3: Remove from List")
+
+    selection = input(": ")
+    if selection == "1":
+        todo_show()
+    elif selection == "2":
+        todo_new()
+    elif selection == "3":
+        # Showing the todo list
+        todo_show()
+        todo_remove()
 
 input()
