b9a3079b2258edf9a90a2c916e90f4fa5f14a17e
Error-PY <55046135+Error-PY@users.noreply.github.com>
2021-06-20 12:17:21 +0200

    Add files via upload

---
+++ todo.py
@@ -0,0 +1,79 @@
+from os import replace
+import time
+
+
+
+
+
+
+print("Welcome to your TODO List")
+time.sleep(1)
+print("Commands:")
+time.sleep(0.25)
+print("1: Show Pending")
+time.sleep(0.25)
+print("2: Add to List")
+time.sleep(0.25)
+print("3: Remove from List")
+selection = input(": ")
+
+
+
+
+
+def todo_show():
+
+    #getting list
+    f = open("./list.txt", 'r')
+
+    #reaing list
+    todolist = f.read()
+    print(todolist)
+    f.close()
+
+
+def todo_new():
+
+    #getting list
+    f = open("./list.txt", 'a')
+
+    new_todo_task = input("Name of new Task\n : ")
+
+    #writing task to newline in file
+    f.write(new_todo_task + "\n")
+    f.close()
+
+def todo_remove():
+
+    #getting list
+    f = open("./list.txt", "r+")
+
+
+    lines = f.read()
+
+
+    #putting contents of file in a list variable
+    todo_list = [lines]
+    f.close()
+    todo_list = str(todo_list)
+
+    #deleting unused characters
+    todo_list = todo_list.replace("n", "\n")
+    todo_list = todo_list.replace('[', "")
+    todo_list = todo_list.replace(']', "")
+    todo_list = todo_list.replace("'", "")
+
+    todo_remove_input = input("TODO Name\n: ")
+    
+    todo_list.replace(todo_remove_input, " ")
+    print(todo_list)
+
+
+#deciding task on selecion
+
+if selection == "1":
+    todo_show()
+elif selection == "2":
+    todo_new()
+elif selection == "3":
+    todo_remove()
